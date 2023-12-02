419e4db198b8f1b0e5334e6497a0e76b48ebfcc1
swayangjit <swayangjit029@gmail.com>
2023-12-02 17:44:16 +0530

    Issue #KKM-000 chore: Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'org.ekstep.djp',
+  appName: 'Khel Khel Mein',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
